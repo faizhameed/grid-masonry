1e485dd2c39399d1d5c61a80539fe0c30e317202
faizhameed <faizftb@gmail.com>
2020-01-08 14:46:09 +0530

    Updates

---
+++ precache-manifest.a3646ea3b0910a307bf054186f4c4d9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cff282c3459f21e3a8078255ddf3a81",
+    "url": "/grid-masonry/index.html"
+  },
+  {
+    "revision": "4c8cd823209409b36a72",
+    "url": "/grid-masonry/static/css/2.faecdb03.chunk.css"
+  },
+  {
+    "revision": "408f6e9a20b25e240c37",
+    "url": "/grid-masonry/static/css/main.29e5b734.chunk.css"
+  },
+  {
+    "revision": "4c8cd823209409b36a72",
+    "url": "/grid-masonry/static/js/2.3805815e.chunk.js"
+  },
+  {
+    "revision": "8586b89c81a042f4d711f03f1a4938ab",
+    "url": "/grid-masonry/static/js/2.3805815e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "408f6e9a20b25e240c37",
+    "url": "/grid-masonry/static/js/main.457de3c5.chunk.js"
+  },
+  {
+    "revision": "84f7bf6bfcc247f90d97",
+    "url": "/grid-masonry/static/js/runtime-main.4753d5e3.js"
+  }
+]);
